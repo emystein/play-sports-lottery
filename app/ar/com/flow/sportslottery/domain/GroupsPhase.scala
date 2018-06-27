142694eb2794b8c9a627836c7e25d4e09ca3c26c
emystein <emystein@gmail.com>
2018-06-27 10:28:26 -0300

    GroupsPhase.getMatchEvent

--- app/ar/com/flow/sportslottery/domain/GroupsPhase.scala
+++ app/ar/com/flow/sportslottery/domain/GroupsPhase.scala
@@ -3,12 +3,19 @@ package ar.com.flow.sportslottery.domain
 import java.util.Date
 
 case class GroupsPhase(groups: Set[Set[MatchEvent]]) {
+
   require(groups.map(group => differentMatchDaysForTheSameTeam(group)).forall(_ == true), "A team can't play two matches the same day")
 
   def differentMatchDaysForTheSameTeam(group: Set[MatchEvent]): Boolean = {
     val teamsWithMoreThanOneMatchTheSameDay = group.toSeq.flatMap(m => Seq(TeamDate(m.homeTeam, m.date), TeamDate(m.visitorTeam, m.date)))
                                                          .groupBy(identity).mapValues(_.size).filter(_._2 > 1)
     teamsWithMoreThanOneMatchTheSameDay.isEmpty
+  }
+
+  def getMatchEvent(team1: String, team2: String): Option[MatchEvent] = {
+    val lookupRivals = Set(team1, team2)
+
+    groups.flatten.filter(matchEvent => Set(matchEvent.homeTeam, matchEvent.visitorTeam) == lookupRivals).headOption
   }
 }
 
--- test/ar/com/flow/sportslottery/domain/GroupsPhaseTest.scala
+++ test/ar/com/flow/sportslottery/domain/GroupsPhaseTest.scala
@@ -4,10 +4,10 @@ import org.specs2.mutable.Specification
 
 class GroupsPhaseTest extends Specification with TestObjects {
 
-  "Groups phase" >> {
+  "Groups phase creation" >> {
+
     "Create a groups phase with two groups" >> {
       val phase = GroupsPhase(Set(groupAMatchSchedules, groupBMatchSchedules))
-
       phase.groups must contain(===(groupAMatchSchedules))
       phase.groups must contain(===(groupBMatchSchedules))
     }
@@ -20,6 +20,32 @@ class GroupsPhaseTest extends Specification with TestObjects {
 
       new GroupsPhase(Set(group)) should throwA(new IllegalArgumentException("requirement failed: A team can't play two matches the same day"))
     }
+  }
 
+  "Group phase matches" >> {
+    val phase = GroupsPhase(Set(groupAMatchSchedules, groupBMatchSchedules))
+
+    "Get a match event given teams in home and visitor order and date" >> {
+      val matchEvent = phase.getMatchEvent("Argentina", "Iceland")
+
+      matchEvent must be equalTo(Some(argentinaIcelandMatch))
+    }
+
+    "Get a match event given teams in visitor and home order and date" >> {
+      val matchEvent = phase.getMatchEvent( "Iceland", "Argentina")
+
+      matchEvent must be equalTo(Some(argentinaIcelandMatch))
+    }
+
+    "Looking up a match event between teams that don't play against each other throws an exception" >> {
+      phase.getMatchEvent( "Germany", "Argentina") must be empty
+    }
+
+//
+//    "Register a match result" >> {
+//      val phase = GroupsPhase(Set(groupAMatchSchedules, groupBMatchSchedules))
+//
+//      val matchResult = phase.registerMatchResult(argentinaIcelandMatch, )
+//    }
   }
 }
