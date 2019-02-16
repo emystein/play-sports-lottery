136865a9fc202bb3e1826b3586d580b7c1020300
Emiliano Menéndez <emystein@gmail.com>
2019-02-15 23:47:09 -0300

    GroupRanking

--- app/ar/com/flow/sportslottery/domain/GroupRanking.scala
+++ app/ar/com/flow/sportslottery/domain/GroupRanking.scala
@@ -1,5 +1,9 @@
 package ar.com.flow.sportslottery.domain
 
 case class GroupRanking(teamRanks: List[TeamRank]) {
+<<<<<<< HEAD
   var teamOrder = teamRanks.map(_.team)
+=======
+  val teamOrder = teamRanks.map(_.team)
+>>>>>>> GroupRanking
 }
--- test/ar/com/flow/sportslottery/domain/GroupsRankingTest.scala
+++ test/ar/com/flow/sportslottery/domain/GroupsRankingTest.scala
@@ -2,12 +2,13 @@ package ar.com.flow.sportslottery.domain
 
 import org.specs2.mutable.Specification
 
-class GroupRankingTest extends Specification with TestObjects {
-  "GroupRanking" >> {
-    "must order teams after adding match result" >> {
+class GroupsRankingTest extends Specification with TestObjects {
+  "Group Ranking" >> {
+    "Add a match result must update order of teams in ranking" >> {
       val ranking = GroupsRanking(groupsPhaseMetadata)
 
       ranking.addMatchResult(croatiaNigeriaMatch, 2, 0)
+      // TODO investigate why without registering this result the ranking sometimes differs from the expected
       ranking.addMatchResult(nigeriaArgentinaMatch, 1, 2)
 
       ranking.getGroupRanking(groupD).teamOrder must be equalTo List("Croatia", "Argentina", "Iceland", "Nigeria")
