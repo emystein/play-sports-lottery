e56db36b3fc985dcc2816c2a33e1cf08166f22fa
Emiliano Menéndez <emystein@gmail.com>
2019-02-16 11:59:47 -0300

    Fix GroupRanking merge issue

--- app/ar/com/flow/sportslottery/domain/GroupRanking.scala
+++ app/ar/com/flow/sportslottery/domain/GroupRanking.scala
@@ -1,13 +1,5 @@
 package ar.com.flow.sportslottery.domain
 
 case class GroupRanking(teamRanks: List[TeamRank]) {
-<<<<<<< HEAD
-<<<<<<< HEAD
   var teamOrder = teamRanks.map(_.team)
-=======
-  val teamOrder = teamRanks.map(_.team)
->>>>>>> GroupRanking
-=======
-  var teamOrder = teamRanks.map(_.team)
->>>>>>> GroupsRanking constructs receiving groups intead of GroupsPhaseMetadata)
 }
