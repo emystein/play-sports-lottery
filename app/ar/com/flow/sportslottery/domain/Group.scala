578c6f95f3538c046ba41cef1db0305be182c503
Emiliano Menéndez <emystein@gmail.com>
2019-02-19 00:07:44 -0300

    Cleaned up Group.

--- app/ar/com/flow/sportslottery/domain/Group.scala
+++ app/ar/com/flow/sportslottery/domain/Group.scala
@@ -1,22 +1,4 @@
 package ar.com.flow.sportslottery.domain
 
-import ar.com.flow.sportslottery.domain.GroupPreconditions.allTeamsPlayAgainstEachOther
+case class Group(name: String, teams: Set[String], matchSchedules: Set[MatchSchedule])
 
-case class Group(name: String, teams: Set[String] = Set.empty, matchSchedules: Set[MatchSchedule]) {
-//  require(allTeamsPlayAgainstEachOther(teams, matchSchedules), "all teams must play against each other")
-}
-
-object GroupPreconditions {
-  def allTeamsPlayAgainstEachOther(teams: Set[String], matchSchedules: Set[MatchSchedule]): Boolean = {
-    val twoSidedTeamPairs = matchSchedules.flatMap(matchSchedule => cartesianPairs(matchSchedule.teams))
-    twoSidedTeamPairs == cartesianPairs(teams)
-  }
-
-  private def cartesianPairs(teams: Set[String]): Set[(String, String)] = {
-    for {
-      team1 <- teams
-      team2 <- teams
-      if team1 != team2
-    } yield (team1, team2)
-  }
-}
--- app/ar/com/flow/sportslottery/domain/GroupsPhaseMetadata.scala
+++ app/ar/com/flow/sportslottery/domain/GroupsPhaseMetadata.scala
@@ -1,7 +1,6 @@
 package ar.com.flow.sportslottery.domain
 
-import ar.com.flow.sportslottery.domain.GroupPreconditions.allTeamsPlayAgainstEachOther
-import ar.com.flow.sportslottery.domain.GroupsPhasePreconditions.{onlyOneGroupForEachTeam, onlyOneMatchPerDayForEachTeam}
+import ar.com.flow.sportslottery.domain.GroupsPhasePreconditions._
 
 case class GroupsPhaseMetadata(groups: Set[Group]) extends GroupedTeams {
   val scheduledMatches: Set[MatchSchedule] = groups.flatMap(_.matchSchedules)
@@ -9,7 +8,7 @@ case class GroupsPhaseMetadata(groups: Set[Group]) extends GroupedTeams {
   require(onlyOneMatchPerDayForEachTeam(scheduledMatches), "A team can't play two matches the same day")
   require(onlyOneGroupForEachTeam(teamsByGroup), "A team can't appear in more than one group")
   groups.foreach { group =>
-    require(allTeamsPlayAgainstEachOther(group.teams, group.matchSchedules), "all teams must play against each other")
+    require(allTeamsInAGroupPlayAgainstEachOther(group.teams, group.matchSchedules), "all teams must play against each other")
   }
 
   def getMatchSchedule(team1: String, team2: String): Option[MatchSchedule] = {
